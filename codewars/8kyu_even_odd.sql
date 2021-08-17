738570c772a16b6e9b2bf6706834e902df68acad
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-08-17 15:39:20 +0300

    Add files via upload

---
+++ codewars/8kyu_even_odd.sql
@@ -0,0 +1,12 @@
+-- SQL Notes:
+-- You will be given a table, numbers, with one column number.
+
+-- Return a table with a column is_even containing "Even" or "Odd" depending on number column values.
+
+-- numbers table schema
+-- number INT
+-- output table schema
+-- is_even STRING
+
+
+select Case when number %2 = 0 then 'Even' else 'Odd' end as is_even from numbers
