35766d20fc339d461fedc3f488a2fbd045f9dc98
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-08-18 15:57:28 +0300

    Update 8kyu_returning_strings.sql

--- codewars/8kyu_returning_strings.sql
+++ codewars/8kyu_returning_strings.sql
@@ -5,19 +5,3 @@
 select
 concat('Hello, ', name, ' how are you doing today?') as greeting
 from person
-
-
--- Clever solutions:
-
-select
-'Hello, ' || name || ' how are you doing today?' as greeting
-from person
-
-
-select
-format('Hello, %s how are you doing today?', name) as greeting
-from person
-
-Select
-( 'Hello, ' || person.name || ' how are you doing today?')  as greeting
-from person
