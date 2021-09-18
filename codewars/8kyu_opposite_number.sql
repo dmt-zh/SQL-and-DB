d1e353844a1dfd110a9c6ca463a20c9998bca6fa
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-09-18 19:43:21 +0300

    Add files via upload

---
+++ codewars/8kyu_opposite_number.sql
@@ -0,0 +1,10 @@
+-- Very simple, given a number, find its opposite.
+-- Examples:
+-- 1: -1
+-- 14: -14
+-- -34: 34
+
+-- You will be given a table: opposite, with a column: number. Return a table with a column: res.
+
+SELECT -number AS res
+FROM opposite;
