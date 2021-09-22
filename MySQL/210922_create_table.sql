bafc4eb8c1fd13b28d4a7a3c92fb05da35fac38d
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-09-22 14:12:25 +0300

    Create 210922_create_table.sql

---
+++ MySQL/210922_create_table.sql
@@ -0,0 +1,19 @@
+-- Сформулируйте SQL запрос для создания таблицы book, занесите  его в окно кода (расположено ниже)  
+-- и отправьте на проверку (кнопка Отправить). Структура таблицы book:
+
+-- Поле	Тип, описание
+-- book_id	INT PRIMARY KEY AUTO_INCREMENT
+-- title	VARCHAR(50)
+-- author	VARCHAR(30)
+-- price	DECIMAL(8, 2)
+-- amount	INT
+
+CREATE TABLE book(
+    book_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(50) NOT NULL,
+    author VARCHAR(30) NOT NULL, 
+    price DECIMAL(8,2) NOT NULL,
+    amount INT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
