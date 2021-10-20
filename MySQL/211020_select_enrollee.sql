193eb18773ec4e96e7c437fa5e5b18864b111ebc
Dmitry Zhigalo <84251312+dmt-zh@users.noreply.github.com>
2021-10-20 21:09:06 +0300

    Add files via upload

---
+++ MySQL/211020_select_enrollee.sql
@@ -0,0 +1,24 @@
+-- Задание
+-- Вывести абитуриентов, которые хотят поступать на образовательную программу
+-- «Мехатроника и робототехника» в отсортированном по фамилиям виде.
+
+SELECT name_enrollee
+FROM enrollee
+	INNER JOIN program_enrollee USING (enrollee_id)
+	INNER JOIN program USING (program_id)
+WHERE name_program = 'Мехатроника и робототехника'
+ORDER BY name_enrollee;
+
+
+-- Query result:
+-- +-----------------+
+-- | name_enrollee   |
+-- +-----------------+
+-- | Баранов Павел   |
+-- | Попов Илья      |
+-- | Семенов Иван    |
+-- | Степанова Дарья |
+-- +-----------------+
+-- Affected rows: 4
+
+
